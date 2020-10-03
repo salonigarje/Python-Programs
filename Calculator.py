edd54b7b271acb621295dbbb10c0cb98c7275421
salonigarje <71958403+salonigarje@users.noreply.github.com>
2020-10-03 13:41:32 +0530

    updated

--- Calculator.py
+++ Calculator.py
@@ -16,8 +16,6 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
     return x / y
-
-
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -32,7 +30,6 @@ while True:
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
